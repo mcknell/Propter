c43ee890dd29a70bffbbf381ce908b8842ffee12
Mark Knell <markkmail-propter@yahoo.com>
2012-09-09 12:35:43 -0700

    Shared assembly versioning.

---
+++ dotnet/source/Propter.Core/Specify.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Propter.Core
+{
+    public static class Specify
+    {
+        public static string /*ISubjectBuilder<THost>*/ For<THost>(Expression<Func<THost>> expression)
+        {
+            return ""/*new SubjectBuilder<THost>()*/;
+        }
+    }
+}
